de05431a605fff34eeb60177faf082808244805e
aditya <adityap92@gmail.com>
2016-12-04 11:54:56 -0500

    Initial Commit

---
+++ app/src/main/java/com/popularmovies/MovieSortActivity.java
@@ -0,0 +1,13 @@
+package com.popularmovies;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MovieSortActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_sort);
+    }
+}
