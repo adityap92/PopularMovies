f8f233d6f958e7689a74f1249a43aa4399daa722
aditya <adityap92@gmail.com>
2016-12-25 21:32:26 -0500

    Added MovieGrid Adapter

--- app/src/main/java/com/popularmovies/MovieSortActivity.java
+++ app/src/main/java/com/popularmovies/MovieSortActivity.java
@@ -2,12 +2,32 @@ package com.popularmovies;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MovieSortActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+
+
+        GridView gridview = (GridView) findViewById(R.id.movieGrid);
+        gridview.setAdapter(new MovieGridAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MovieSortActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
         setContentView(R.layout.activity_movie_sort);
     }
 }
